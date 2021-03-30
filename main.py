55ba1553255f26b78322c7aea9af50f72567b437
SamMc98 <sammcphee87@gmail.com>
2021-03-30 21:27:39 +0000

    Added a division method. Still not complete, but basically functionality is added.

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+Remainders = []
+Answer = []
+
 def headerValue(baseNum, rangeNum):
   print("Header values in base", baseNum, "up to", rangeNum, ":")
   for powerRange in range(rangeNum):
@@ -23,19 +26,49 @@ def multiply(NumberToConvert, baseNumber):
   #each digit in 1454 must be < 7
   #try/except OR if else
 
-def divide():
-  print("Higher base to lower base.")
+
+def divide(Num, Base):
+  #print("Higher base to lower base.")
+  Div = Num
+  if (int(Num) >= 1):
+   Div = Num / Base
+   Mod = Num % Base
+   Num = int(Div)
+   Remainders.append(int(Mod))
+   divide(Div, Base)
+  else:
+    Mod = Div
+    Div = 0
+    count = Remainders.__len__()
+    for i in range(count):
+     count = count - 1
+     Answer.append(Remainders[count])
+    printDivAns(Remainders, Answer)
+
+def printDivAns(Remainders, Answer):
+    x = ""
+    for i in Answer:
+      x += str(i)
+    print("Answer: ",x)
 
 #Main
+#multiply
 print("Enter the Number")
 numberToConvertInput = int(input())
 print("Enter a base number:")
 baseNumberInput = int(input())
 multiply(numberToConvertInput, baseNumberInput)
 
-
+#headerValue
 print("Enter a base number:")
 baseNumInput = int(input())
 print("Enter a range to not exceed:")
 rangeInput = int(input())
 headerValue(baseNumInput, rangeInput) #baseNumber, range
+
+#divide
+print("Enter the number")
+Num = int(input())
+print("Enter the base number")
+Base = int(input())
+divide(Num, Base)
